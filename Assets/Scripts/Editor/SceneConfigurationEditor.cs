34f331e188b6c2eb88d1ab6a673cf134b5713495
BeardedPlatypus <mwtegelaers@gmail.com>
2022-03-15 09:13:47 -0700

    Clean up SceneConfigurationEditor

--- Assets/Scripts/Editor/SceneConfigurationEditor.cs
+++ Assets/Scripts/Editor/SceneConfigurationEditor.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using BeardedPlatypus.MultipleSceneManager.Runtime;
 using UnityEditor;
 using UnityEditorInternal;
@@ -6,23 +5,33 @@ using UnityEngine;
 
 namespace BeardedPlatypus.MultipleSceneManager.Editor
 {
+    /// <summary>
+    /// <see cref="SceneConfigurationEditor"/> provides the custom inspector for the <see cref="SceneConfiguration"/>.
+    /// </summary>
     [CustomEditor(typeof(SceneConfiguration))]
     public class SceneConfigurationEditor : UnityEditor.Editor
     {
         private const string CheckPath = "Assets/Sprites/check_green.png";
         private const string WarningPath = "Assets/Sprites/warning_yellow.png";
-        private const string ErrorPath = "Assets/Sprites/warning_red.png";
+
+        private static readonly float IconDimension = EditorGUIUtility.singleLineHeight - 4.0F;
+        private const float IconPadding = 1.0F;
+        private const float VerticalSpacing = 1.0F;
+        private const float HorizontalSpacing = 4.0F;
 
         private Texture2D _checkTex;
         private Texture2D _warningTex; 
-        private Texture2D _errorTex;
             
         private ReorderableList _scenes;
 
         private void OnEnable()
         {
-            Texture2D t = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Textures/texture.jpg", typeof(Texture2D));
-            
+            InitializeTextures();
+            InitializeReorderableList();
+        }
+
+        private void InitializeReorderableList()
+        {
             _scenes = new ReorderableList(serializedObject, 
                                           serializedObject.FindProperty("scenes"), 
                                           true, 
@@ -32,12 +41,13 @@ namespace BeardedPlatypus.MultipleSceneManager.Editor
             {
                 drawElementCallback = DrawSceneElement
             };
-
-            _checkTex = (Texture2D) AssetDatabase.LoadAssetAtPath(CheckPath, typeof(Texture2D));
-            _warningTex = (Texture2D) AssetDatabase.LoadAssetAtPath(WarningPath, typeof(Texture2D));
-            _errorTex = (Texture2D) AssetDatabase.LoadAssetAtPath(ErrorPath, typeof(Texture2D));
         }
 
+        private void InitializeTextures()
+        {
+            _checkTex = (Texture2D) AssetDatabase.LoadAssetAtPath(CheckPath, typeof(Texture2D));
+            _warningTex = (Texture2D) AssetDatabase.LoadAssetAtPath(WarningPath, typeof(Texture2D));
+        }
 
         public override void OnInspectorGUI()
         {
@@ -53,28 +63,40 @@ namespace BeardedPlatypus.MultipleSceneManager.Editor
 
         private void DrawSceneElement(Rect rect, int index, bool isActive, bool isFocused)
         {
-            rect.y += 2;
+            rect.y += VerticalSpacing;
             
             var sceneElement = _scenes.serializedProperty.GetArrayElementAtIndex(index);
+            DrawPropertyField(rect, sceneElement, index);
+            DrawIcon(rect, sceneElement);
+        }
 
-            Rect sceneNameFieldRect = 
-                new Rect(rect.x, 
-                         rect.y, 
-                         rect.width - EditorGUIUtility.singleLineHeight - 9, 
-                         EditorGUIUtility.singleLineHeight);
+        private void DrawPropertyField(Rect parentRect, SerializedProperty sceneElement, int index)
+        {
+            Rect sceneNameFieldRect = new Rect(parentRect.x, 
+                                               parentRect.y, 
+                                               GetPropertyFieldWidth(parentRect.width), 
+                                               EditorGUIUtility.singleLineHeight);
             var label = new GUIContent($"Element {index}");
             EditorGUI.PropertyField(sceneNameFieldRect, sceneElement, label);
+        }
+        
+        private static float GetPropertyFieldWidth(float parentWidth) =>
+            parentWidth - IconDimension - 2 * IconPadding - HorizontalSpacing;
 
+        private void DrawIcon(Rect parentRect, SerializedProperty sceneElement)
+        {
+            // Do not draw an icon if the field is empty.
             if (string.IsNullOrWhiteSpace(sceneElement.stringValue))
             {
                 return;
             }
-            
+
+            float iconOffset = GetPropertyFieldWidth(parentRect.width) + HorizontalSpacing;
             Rect iconRect = 
-                new Rect(rect.x + (rect.width - EditorGUIUtility.singleLineHeight - 5) + 2, 
-                         rect.y + 2, 
-                         EditorGUIUtility.singleLineHeight + 1, 
-                         EditorGUIUtility.singleLineHeight - 4);
+                new Rect(parentRect.x + iconOffset + IconPadding, 
+                         parentRect.y + IconPadding, 
+                         IconDimension, 
+                         IconDimension);
 
             EditorGUI.LabelField(iconRect,
                 ShouldWarn(sceneElement.stringValue) 
@@ -84,6 +106,5 @@ namespace BeardedPlatypus.MultipleSceneManager.Editor
 
         private static bool ShouldWarn(string sceneName) =>
             !SceneUtils.IsInBuildSettings(sceneName);
-
     }
 }
