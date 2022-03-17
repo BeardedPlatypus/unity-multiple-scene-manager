ac3d656cb9a170a61fca9ccb865597940ae978eb
Maarten Tegelaers <BeardedPlatypus@users.noreply.github.com>
2022-03-16 22:26:54 -0700

    fix(docs): extend documentation scene configuration (#9)
    Add additional documentation to the SceneConfiguration and SceneUtils classes.

--- Assets/Scripts/Runtime/SceneConfiguration.cs
+++ Assets/Scripts/Runtime/SceneConfiguration.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 namespace BeardedPlatypus.MultipleSceneManager.Runtime
 {
+    /// <summary>
+    /// <see cref="SceneConfiguration"/> describes a set of strings which should be loaded as a single collection.
+    /// </summary>
     [CreateAssetMenu(fileName="MultipleSceneConfiguration",
         menuName="BeardedPlatypus/Scenes/MultipleSceneConfiguration")]
     [Serializable]
     public sealed class SceneConfiguration : ScriptableObject
     {
+        /// <summary>
+        /// The scenes that should be loaded as a single collection.
+        /// </summary>
         public string[] scenes = {};
     }
 }
